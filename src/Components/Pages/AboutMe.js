b216844280ef35d5677dde50036bd5124dea36e2
Alex Canez <el.winy.36@gmail.com>
2022-07-24 11:56:57 -0600

    added features

--- src/Components/Pages/AboutMe.js
+++ src/Components/Pages/AboutMe.js
@@ -1,10 +1,22 @@
 import Header from "../Header";
+import React from 'react';
+//import coverImage from '';
 
 function AboutMe() {
-
-    return(
+    return (
         <div>
-            <Header title="AboutMe" subtitle="Full stack web developer"/>
+            <Header title="AboutMe" subtitle="Hello ," />
+            <section>
+                <p>
+                    <p>I'am Alex Canez , Web developer & Sales Manager. Originally from California but i called Utah my home for the last 20 years.</p>
+                    <p>I've been doing telecomunications sales for the last 10 years with great success and customer service for 5 years prior.</p>
+                    <p>And now im ready for this new and exciting career as a web developer.</p>
+                    <p>Feel free to message me with your questions and inquires about my services.</p>
+                    <button type="submit">
+                        Im also Bilingual
+                    </button>
+                </p>
+            </section>
         </div>
     )
 }
--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,10 +1,15 @@
 import Header from "../Header";
 
 function Projects() {
-
-    return(
+    return (
         <div>
-            <Header title="Projects" subtitle="My recent projects"/>
+            <Header title="Projects" subtitle="Look at some of my latest projects. 👇" />
+            <h3>Tattoo Appt Booker</h3>
+            <h3>Petsagram</h3>
+            <h3>Budget Tracker</h3>
+            <h3>Note Taker</h3>
+            <h3>Weather Dashboard API</h3>
+            <h3>Password Generator</h3>
         </div>
     )
 }
