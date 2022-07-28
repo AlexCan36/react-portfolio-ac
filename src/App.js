6a748efd2ed75b74c4c3f961ec85290a43a66ebd
Alex Canez <el.winy.36@gmail.com>
2022-07-28 10:40:11 -0600

    added resume section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header';
+//import Header from './Components/Header';
 import { useState } from 'react';
 import AboutMe from './Components/Pages/AboutMe';
 import Projects from './Components/Pages/Projects';
 import Contact from './Components/Pages/Contact';
+import Resume from './Components/Pages/Resume';
 
 function App() {
   const [currentPage, setcurrentPage] = useState('About Me')
@@ -15,15 +16,16 @@ function App() {
           <li onClick={() => setcurrentPage("About Me")}>About Me</li>
           <li onClick={() => setcurrentPage("Projects")}>Projects</li>
           <li onClick={() => setcurrentPage("Contact")}>Contact</li>
-          <li>Resume</li>
+          <li onClick={() => setcurrentPage("Resume")}>Resume</li>
         </ul>
       </nav>
       {currentPage === "About Me" ? <AboutMe />
         : currentPage === "Projects" ? <Projects />
+        : currentPage === "Resume" ? <Resume />
           : <Contact />}
 
       <footer>
-        <h2>Links:</h2>
+        <h2>footer:</h2>
       </footer>
 
     </div>
---
+++ src/Components/Pages/Resume.js
@@ -0,0 +1,23 @@
+import Header from "../Header";
+import React from 'react';
+//import coverImage from '';
+
+function Resume() {
+    return (
+        <div>
+            <Header title="Resume" />
+            <h2> Download my Resume </h2>
+            <section>
+                <ul>
+                    <li> a</li>
+                    <li> a</li>
+                    <li>a </li>
+                    <li>a </li>
+                    <li>a </li>
+                </ul>
+            </section>
+        </div>
+    )
+}
+
+export default Resume;
