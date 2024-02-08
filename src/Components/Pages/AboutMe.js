3618bd619125c79db0f669345451fdaa9b7c7d73
Alex Canez <el.winy.36@gmail.com>
2024-02-07 20:30:31 -0700

    change about me

--- src/Components/Pages/AboutMe.js
+++ src/Components/Pages/AboutMe.js
@@ -4,14 +4,14 @@ import React from 'react';
 function AboutMe() {
     return (
         <div>
-            <Header title="About Me" subtitle="Hi ," />
+            <Header title="About Me" subtitle="Welcome ," />
             <body>
                 <section class="hero">
                     <div class="hero-cta">
-                        <p>I'am Alex Canez , Web developer & Sales Manager. Originally from California but i called Utah my home for the last 20 years.</p>
-                        <p>I've been doing telecomunications sales for the last 10 years with great success and customer service for 5 years prior.</p>
-                        <p>And now im ready for this new and exciting career as a web developer.</p>
-                        <p>Feel free to message me with your questions and inquires about my services.</p>
+                        <p>Hello, I'm Alex Canez, a skilled Web Developer and Sales Manager. My journey in web development began in early 2021, and since then, I've continually honed my skills, culminating in the attainment of my diploma from THE U.</p>
+                        <p>Now, as a certified, team-oriented web developer, I am eager to contribute to your team by delivering clean, efficient code to drive successful projects. I am committed to ongoing growth and thrive on the challenge of mastering new technologies.</p>
+                        <p>What sets me apart is my unwavering passion for coding, coupled with a relentless drive to exceed expectations and support my team. No challenge is too daunting for me to tackle head-on.</p>
+                        <p>Feel free to reach out with any questions or inquiries about my services. I look forward to the opportunity to collaborate with you.</p>
                     </div>
                 </section>
             </body>
