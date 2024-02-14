a887d6044ba0b0c13eef7485142576163a7322c8
Alex Canez <el.winy.36@gmail.com>
2024-02-13 23:55:02 -0700

    added event on clicks

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ import Contact from './Components/Pages/Contact';
 import Resume from './Components/Pages/Resume';
 
 function App() {
-  const [currentPage, setcurrentPage] = useState('About Me')
+  const [currentPage, setcurrentPage] = useState('About Me');
+
+  const handleRefresh = () => {
+    window.location.reload(); // Reload the page
+  };
   return (
     <div>
       <nav>
         <ul style={{ display: 'flex', listStyle: 'none' }}>
-          <li>Alex Canez</li>
+        <li onClick={handleRefresh}>Alex Canez</li> {/* Add onClick event handler */}
           <li onClick={() => setcurrentPage("About Me")}>About Me</li>
           <li onClick={() => setcurrentPage("Projects")}>Projects</li>
           <li onClick={() => setcurrentPage("Contact")}>Contact</li>
@@ -32,15 +36,15 @@ function App() {
         <div class='row'>
         <div class="column">
             <h5>Github</h5>
-            <a target='_blank' href="https://github.com/AlexCan36"><img border="1" alt="Github" src={githubLogo} width="100" height="100"></img></a>
+            <a target='_blank'rel='noreferrer' href="https://github.com/AlexCan36"><img border="1" alt="Github" src={githubLogo} width="100" height="100"></img></a>
           </div>
           <div class="column">
           <h5>Linkedin</h5>
-            <a target='_blank' href="https://www.linkedin.com/in/alex-canez-050823230/"><img border="1" alt="Linkedin" src={linkedinLogo} width="100" height="100"></img></a>
+            <a target='_blank'rel='noreferrer' href="https://www.linkedin.com/in/alex-canez-050823230/"><img border="1" alt="Linkedin" src={linkedinLogo} width="100" height="100"></img></a>
           </div>
           <div class="column">
           <h5>Facebook</h5>
-            <a target='_blank' href="https://www.facebook.com/elwinny.canez/"><img border="1" alt="Facebook" src={facebookLogo} width="100" height="100"></img></a>
+            <a target='_blank' rel='noreferrer' href="https://www.facebook.com/elwinny.canez/"><img border="1" alt="Facebook" src={facebookLogo} width="100" height="100"></img></a>
           </div>
         </div>
       </footer>
--- src/Components/Pages/AboutMe.js
+++ src/Components/Pages/AboutMe.js
@@ -7,7 +7,7 @@ function AboutMe() {
             <Header title="About Me" subtitle="Welcome ," />
             <body>
                 <section class="hero">
-                    <div class="hero-cta">
+                <div class="hero-cta" style={{textAlign: 'left'}}> 
                         <p>Hello, I'm Alex Canez, a skilled Web Developer and Sales Manager. My journey in web development began in early 2021, and since then, I've continually honed my skills, culminating in the attainment of my diploma from THE U.</p>
                         <p>Now, as a certified, team-oriented web developer, I am eager to contribute to your team by delivering clean, efficient code to drive successful projects. I am committed to ongoing growth and thrive on the challenge of mastering new technologies.</p>
                         <p>What sets me apart is my unwavering passion for coding, coupled with a relentless drive to exceed expectations and support my team. No challenge is too daunting for me to tackle head-on.</p>
